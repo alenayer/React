4954a65dbbcd91f0fc84fcb4e70fc9dff1daebb8
alenayer <al.yerokhina@gmail.com>
2025-06-11 12:52:29 +0300

    merge app2

--- yerokhina-react-app/src/main.tsx
+++ yerokhina-react-app/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import App2 from './App2.tsx'
+import { App3 } from './App3.tsx'
+import App4 from './App4';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App2 />
+    <App4 />
   </StrictMode>,
 )
